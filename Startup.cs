616a5f54a2f8acdcb68c90d33c36b2ef751d3ac3
sreekanth167 <58954791+sreekanth167@users.noreply.github.com>
2019-12-18 12:06:54 -0500

    netcode 3

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace sk_git
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello there!");
                 });
             });
         }
